8bcb8bd3e3466bdcd5aa2d554db72650a0d88194
umnni <manish@umnni.com>
2026-09-02 13:01:57 +0530

    try to solve pages link issue

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ function PageLayout({ children }) {
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/DesignInterior">
       <Routes>
         <Route
           path="/"
